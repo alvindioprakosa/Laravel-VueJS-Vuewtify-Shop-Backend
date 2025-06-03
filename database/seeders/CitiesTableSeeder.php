1c072891626aa85f15ab9899a70ca9bf1dbe33eb
alvindioprakosa <alvindioprakosa@gmail.com>
2025-06-03 09:30:54 +0700

    Update Cities Table Seeder

--- database/seeders/CitiesTableSeeder.php
+++ database/seeders/CitiesTableSeeder.php
@@ -15,20 +15,20 @@ class CitiesTableSeeder extends Seeder
      * @return void
      */
     public function run()
-        {
-            $url_city = "https://api.rajaongkir.com/starter/city?key=c82e4567ef2c0a91100fa35077143ae9";
-            $json_str = file_get_contents($url_city);
-            $json_obj = json_decode($json_str);
-            $cities = [];
-            foreach($json_obj->rajaongkir->results as $city){
-                $cities[] = [
-                    'id'            => $city->city_id,
-                    'province_id'   => $city->province_id,
-                    'city'     => $city->city_name,
-                    'type'          => $city->type,
-                    'postal_code'   => $city->postal_code,
-                ];
-            }
-            DB::table('cities')->insert($cities);
+    {
+        $url_city = "https://api.rajaongkir.com/starter/city?key=c82e4567ef2c0a91100fa35077143ae9";
+        $json_str = file_get_contents($url_city);
+        $json_obj = json_decode($json_str);
+        $cities = [];
+        foreach ($json_obj->rajaongkir->results as $city) {
+            $cities[] = [
+                'id'            => $city->city_id,
+                'province_id'   => $city->province_id,
+                'city'     => $city->city_name,
+                'type'          => $city->type,
+                'postal_code'   => $city->postal_code,
+            ];
         }
+        DB::table('cities')->insert($cities);
+    }
 }
