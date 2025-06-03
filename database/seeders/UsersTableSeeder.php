9a488ce6700b380c7fe08867fe43ee52fe06f8a6
alvindioprakosa <alvindioprakosa@gmail.com>
2025-06-03 09:30:41 +0700

    Update User Table Seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,19 +15,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Anwar',
-            'email' => 'anwar@gmail.com',
-            'password' => bcrypt('123456'),
-            'roles' => json_encode(['CUSTOMER']),
-            'status' => 'ACTIVE',
-        ],
-        [
-            'name' => 'Budi',
-            'email' => 'budi@gmail.com',
-            'password' => bcrypt('123456'),
-            'roles' => json_encode(['CUSTOMER']),
-            'status' => 'ACTIVE',
-        ]);
+        DB::table('users')->insert(
+            [
+                'name' => 'Anwar',
+                'email' => 'anwar@gmail.com',
+                'password' => bcrypt('123456'),
+                'roles' => json_encode(['CUSTOMER']),
+                'status' => 'ACTIVE',
+            ],
+            [
+                'name' => 'Budi',
+                'email' => 'budi@gmail.com',
+                'password' => bcrypt('123456'),
+                'roles' => json_encode(['CUSTOMER']),
+                'status' => 'ACTIVE',
+            ]
+        );
     }
 }
