5d04e9adf5b82cf300e635704ba48f4fc51f86ac
alvindioprakosa <alvindioprakosa@gmail.com>
2025-06-03 09:30:31 +0700

    Update Provinces Table Seeder

--- database/seeders/ProvincesTableSeeder.php
+++ database/seeders/ProvincesTableSeeder.php
@@ -19,7 +19,7 @@ class ProvincesTableSeeder extends Seeder
         $json_str = file_get_contents($url_province);
         $json_obj = json_decode($json_str);
         $provinces = [];
-        foreach($json_obj->rajaongkir->results as $province){
+        foreach ($json_obj->rajaongkir->results as $province) {
             $provinces[] = [
                 'id' => $province->province_id,
                 'province' => $province->province
